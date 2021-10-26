dd94c4718a016d33adb7e669372abb04d47bdba8
Edwin <edwinzhou99@gmail.com>
2021-10-26 14:06:59 -0700

    add q26

--- app.js
+++ app.js
@@ -159,3 +159,15 @@ var removeDuplicates = function(nums){
     }
     return i+1;
 }
+
+//26 Remove Element
+var removeElement = function(nums, val){
+    let count = 0;
+    for(let i =0; i>nums.length; i++){
+        if(nums[i]!==val){
+            nums[count]=nums[i];
+            count++;
+        }
+    }
+    return count;
+}
