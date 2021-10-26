7ce80fb552124837b9ea209a6b4841999eba9fff
Edwin <edwinzhou99@gmail.com>
2021-10-25 22:39:27 -0700

    add q 13

--- app.js
+++ app.js
@@ -21,3 +21,69 @@ var isPalindrome = function(x){
     }
     return x === reverse || x === ~~(reverse/10);
 }
+
+//13 Roman to Integer
+var romanToInt = function(s){
+    let num=0;
+    for(i=0;i<s.length; i++){
+        if(s[i] === 'I'){
+            if(s[i+1] === 'V'){
+                num+=4;
+                i++;
+                continue;
+            }
+             if(s[i+1] === 'X'){
+                num+=9;
+                i++;
+                continue;
+            }
+             else{
+                 num+=1;
+             }
+        }
+
+        if(s[i] === 'X'){
+            if(s[i+1] === 'L'){
+                num+=40;
+                i++;
+                continue;
+            }
+             if(s[i+1] === 'C'){
+                num+=90;
+                i++;
+                continue;
+            }
+             else{
+                 num+=10;
+             }
+        }
+
+        if(s[i] === 'C'){
+            if(s[i+1] === 'D'){
+                num+=400;
+                i++;
+                continue;
+            }
+             if(s[i+1] === 'M'){
+                num+=900;
+                i++;
+                continue;
+            }
+             else{
+                 num+=100;
+             }
+        }
+        if(s[i]==='V'){
+            num+=5;
+        }
+        if(s[i]==='L'){
+            num+=50;
+        }
+        if(s[i]==='D'){
+            num+=500;
+        }
+        if(s[i]==='V'){
+            num+=1000;
+        }
+    }
+}
