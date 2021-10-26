2df17a89f4e83176ebdfa4b280af327fb54ab9a5
Edwin <edwinzhou99@gmail.com>
2021-10-25 23:08:01 -0700

    add q21

--- app.js
+++ app.js
@@ -130,3 +130,15 @@ var isValid = function(s){
     }
     return stack.length === 0;
 }
+
+//21 Merge Two Sorted Lists
+var mergeTwoLists = function(l1, l2){
+    if(!l1) return l2;
+    if(!l2) return l1;
+    if(l1.val < l2val){
+        l1.next = mergeTwoLists(l2.next, l1);
+        return l1;
+    }
+    l2.next=mergeTwoLists(l2.next, l1);
+    return l2;
+}
