4ddef2067c9b50811cb3984f44a5200b7c2e46b9
Edwin <edwinzhou99@gmail.com>
2021-10-28 13:16:28 -0700

    add q69

--- app.js
+++ app.js
@@ -239,3 +239,43 @@ var plusOne = function(digits){
 }
 
 //67 Add Binary
+var addBinary = function(a,b){
+    let result = '';
+    let i = a.length -1;
+    let j = a.length -1;
+
+    let carry = 0;
+
+    while(i>= 0 || j>=0){
+        let sum = carry;
+        if(i>=0){
+            sum+=a[i--] - '0';
+        }
+        if(j>=0){
+            sum+=b[j--] - '0';
+        }
+
+        result = sum%2+result;
+        carry - parseInt(sum/2);
+    }
+
+    if(carry > 0){
+        result = 1+result;
+    }
+    return result;
+}
+
+//69 sqrt(x)
+var mySqrt = function(x){
+    if(x<2) return x;
+
+    for(let i = 1; i < x; i++){
+        if(i*i ==x)return i;
+        else{
+            if(i*i < x){
+                let next = i+1;
+                if(next*next > x) return i;
+            }
+        }
+    }
+}
