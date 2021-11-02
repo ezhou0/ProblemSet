e9456ccb79961408c466a343298a910a2b58b95d
Edwin <edwinzhou99@gmail.com>
2021-11-01 21:02:22 -0700

    add q83

--- app.js
+++ app.js
@@ -293,3 +293,16 @@ var climbStairs = function(n){
     }
     return countingFunc(n, {});
 }
+
+//83 Remove Duplicates from Sorted List
+var deleteDuplicates = function(head){
+    let current = head;
+    while(current !== null && current.next !== null){
+        if(current.val === current.next.val){
+            current.next = current.next.next
+        }else{
+            current = current.next
+        }
+        return head;
+    }
+}
