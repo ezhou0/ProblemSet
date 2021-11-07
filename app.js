71198a903ed915218f3d9660a732ac919875382e
Edwin <edwinzhou99@gmail.com>
2021-11-06 18:06:29 -0700

    add q108

--- app.js
+++ app.js
@@ -380,3 +380,21 @@ var maxDepth = function(root) {
     return max + 1;
 
 };
+
+//108 convert sorted array to binary search tree
+var sortedArrayToBST = function(nums) {
+    if(nums.length === 0 )return null;
+    if(nums.length === 1) return new TreeNode(nums[0]);
+
+    let centerIdx = Math.floor(nums.length/2);
+    let root = new TreeNode(nums[centerIdx]);
+
+    let leftSubtree = nums.slice(0,centerIdx);
+    let rightSubtree = nums.slice(centerIdx+1, nums.length);
+
+    root.left = sortedArrayToBST(leftSubtree);
+    root.right = sortedArrayToBST(rightSubtree);
+
+    return root;
+}
+
