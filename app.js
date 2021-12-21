bd9891be2319bd3087fa311502c6fbd4384193c9
Edwin <edwinzhou99@gmail.com>
2021-12-21 18:01:35 -0500

    add q2

--- app.js
+++ app.js
@@ -11,6 +11,34 @@ var twoSum = function(nums, target) {
     }
 };
 
+//2 Add Two Numbers
+var addTwoNumbers = function (l1, l2) {
+    let sum = 0;
+    let current = new ListNode(0);
+    let result = current;
+
+    while (l1 || l2) {
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        sum = sum > 9 ? 1 : 0
+
+    }
+
+    if (sum) {
+        current.next = new ListNode(sum);
+    }
+    return result.next;
+};
+
 //9 Palindrome Number
 var isPalindrome = function(x){
     if(x<0){
