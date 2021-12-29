9dde1a1c8d69e9872f86eb831cb6936d8024194e
Edwin <edwinzhou99@gmail.com>
2021-12-29 12:35:03 -0500

    add q12

--- app.js
+++ app.js
@@ -181,6 +181,21 @@ var isPalindrome = function(x){
     }
     return x === reverse || x === ~~(reverse/10);
 }
+//12 integer to roman
+var intToRoman = function(num) {
+    
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+    var roman = ''
+    var i;
+  for ( i in lookup ) {
+    while ( num >= lookup[i] ) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
+    
+};
 
 //13 Roman to Integer
 var romanToInt = function(s){
