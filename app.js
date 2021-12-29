7d8ff94ebf818fa9052139fe9061e3c5005cbe26
Edwin <edwinzhou99@gmail.com>
2021-12-29 16:13:45 -0500

    add q17

--- app.js
+++ app.js
@@ -338,6 +338,26 @@ var threeSumClosest = function(nums, target) {
     
 };
 
+//17 phone number
+var letterCombinations = function(digits) {
+    const L = {"2": "abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+    
+   let len = digits.length, ans = []
+    if (!len) return []
+    
+    const bfs = (pos, str) => {
+        if (pos === len) ans.push(str)
+        else {
+            let letters = L[digits[pos]]
+            for (let i = 0; i < letters.length; i++)
+                bfs(pos+1,str+letters[i])
+        }
+    }
+    bfs(0,"")
+    return ans
+
+};
+
 //20 Valid Parenthesis
 var isValid = function(s){
     const stack = [];
