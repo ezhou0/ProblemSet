dd22288b39487160681d60b9c8d602fa1da17b9f
Edwin <edwinzhou99@gmail.com>
2022-01-04 10:03:59 -0800

    add q38

--- app.js
+++ app.js
@@ -636,6 +636,30 @@ var searchInsert = function(nums, target){
     }
 }
 
+//38 count and say
+var countAndSay = function(n) {
+    let s = '1';
+
+  while (n > 1) {
+    let count = 0;
+    let next = '';
+
+    for (let i = 0; i <= s.length; i++) {
+      if (i === s.length || (i > 0 && s[i] !== s[i - 1])) {
+        next += count + s[i - 1]; // Say
+        count = 0;
+      }
+
+      count++; // Count
+    }
+
+    s = next;
+    n--;
+  }
+
+  return s;
+};
+
 //53 Maximum Subarray
 var maxSubArray = function(nums){
     if(nums.length === 1) return nums[0];
