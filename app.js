ef3a7a4987eb744c16424accf37ce50bbde53028
Edwin <edwinzhou99@gmail.com>
2022-01-07 08:35:37 -0800

    add q46

--- app.js
+++ app.js
@@ -712,6 +712,31 @@ var jump = function(nums) {
     return jump;
 };
 
+//46 permutations
+var permute = function(nums) {
+    var res = [];
+    dfs(res, [], nums);
+    return res;
+};
+
+var dfs = function(res, arr, nums){
+    let len = nums.length;
+    let tmp1 = null;
+    let tmp2 = null;
+    
+    if(!len) return res.push(arr);
+    
+    for(let i = 0; i < len; i++){
+        tmp1 = Array.from(arr);
+        tmp1.push(nums[i]);
+        
+        tmp2 = Array.from(nums);
+        tmp2.splice(i, 1);
+        
+        dfs(res, tmp1, tmp2)
+    }
+};
+
 //53 Maximum Subarray
 var maxSubArray = function(nums){
     if(nums.length === 1) return nums[0];
