7c89f3254c3d2ae6ebb4f0b73ce56740a0b0bd8c
Edwin <edwinzhou99@gmail.com>
2023-05-30 15:53:26 -0700

    add 2

--- newset.js
+++ newset.js
@@ -11,3 +11,36 @@ var twoSum = function(nums, target){
     }
     return null;
 }
+
+//2. add two numbers
+var addTwoNumbers = function(l1, l2){
+    var List = new ListNode(0);
+    var head = List;
+
+    var sum = 0;
+    var carry = 0;
+
+    while(l1 != null || l2 != null || sum>0){
+        if(l1 != null){
+            sum = sum +l1.val;
+            l1 = l1.next;
+        }
+        if(l2 != null){
+            sum = sum+l2.val;
+            l2 = l2.next;
+        }
+        if(sum > 9){
+            sum = sum -10;
+            carry = 1;
+        }
+
+        var next = new ListNode(sum);
+        head.next = next;
+        head = head.next;
+
+        sum = carry;
+        carry = 0;
+    }
+
+    return List;
+}
