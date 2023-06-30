89abb7e591e7dfb8a5a44ef2cb52ba55e948663a
Edwin <edwinzhou99@gmail.com>
2023-06-29 23:15:01 -0700

    complete 94

--- newset.js
+++ newset.js
@@ -348,3 +348,29 @@ var isValid = function(s){
 }
 
 //94 Binary tree in order traversal
+var inorderTraversal = function(root){
+    let tourist = root;
+    let solution = [];
+
+    while(tourist !== null){
+        let guide = tourist.left;
+
+        if(tourist.left !== null){
+            while(guide.right!==null && guide.right !== tourist){
+                guide = guide.right;
+            }
+            if(guide.right === null){
+                guide.right = tourist;
+                tourist = tourist.left;
+            }else{
+                guide.right = null;
+                solution.push(tourist.val);
+                tourist = tourist.right;
+            }
+        }else{
+            solution.push(tourist.val);
+            tourist = tourist.right;
+        }
+    }
+    return solution;
+}
